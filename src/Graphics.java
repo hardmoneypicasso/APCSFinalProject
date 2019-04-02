275287e362801a4ec9aad2b575ccaadc3e499e77
hardmoneypicasso <49205824+hardmoneypicasso@users.noreply.github.com>
2019-04-02 15:33:31 -0400

    Graphics constructor, initial creation of DRIVER.JAVA

---
+++ src/Graphics.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+public class Graphics {
+	
+	public Graphics() {
+		JFrame jf= new JFrame();
+		jf.setSize(1600, 1600);
+		jf.setResizable(false);
+		jf.setVisible(true);
+	}
+	
+}
