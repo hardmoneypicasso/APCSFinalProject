cb44167adfc0eb0254cc08efc6934da3fc019aee
rudytheduck <49205958+rudytheduck@users.noreply.github.com>
2019-04-23 12:46:08 -0400

    no idea what this save is

--- src/Tile.java
+++ src/Tile.java
@@ -1,10 +1,12 @@
 //Cesar
 public interface Tile {
+	
 	//this method counts the number of tiles of a certain color (to be implemented in each team class)
 	public int countNumOfTiles();
 	
-	
-	
+	//this method checks if there is a different colored tile  next to a tile
+	public void checkNextTile();
+
 	
 	
 	
